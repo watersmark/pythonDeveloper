1c2fb572b5a05e8533a4537e6eac218c39eee9e5
watersmark <watersmark@mail.ru>
2020-12-09 16:21:04 +0300

    add chapter3 new theory

--- Coursera/Practise/main.py
+++ Coursera/Practise/main.py
@@ -1,49 +1,26 @@
-# рассмотрим работу со строками как их задавать
-# и так же как их экранировать
+# методы строк
+quote = "покажите мне код"
 
-# Сырые строки обозначаются r
-# и убирают экранирование символов, оставляя их как
-# просто символы в строке
-example_str = r"C:\\work\\"
-print(example_str)
+# метод нахождения колличества букв в строке
+print(quote.count('о'))
 
-# строки можно переносить
-# для этого используем обратный слеш
-str2 = "this text is this text " \
-       "this text is this text" \
-       "this text is this text"
-print(str2)
+# сделать первую букву большой
+print(quote.capitalize())
 
-# для написания большого блока кода
+# позволяет проверить является ли строка числом
+tempStr = '2017'
+print(tempStr.isdigit())
 
-str3 = """
-        this text is this text 
-        this text is this text
-        this text is this text
-"""
-print(str3)
+# оператор in позволяет проверить содержится ли
+# подстрока в строке
+tempStr = 'Hello'
+print('lo' in tempStr)
 
-# строки можно объединять
-# строки неизменяемые при сложении двух строк
-# создаётся новый объект в памяти
-# это можно узнать по адресу в памяти
-str4 = "str4"
-print(id(str4))
-str4 += 'a'
-print(id(str4))
+# преобразования строк
+num_string = str(999.01)
+print(num_string, type(num_string))
 
-# срезы строк
-# [start:stop:step]
-# срез может идти справа налево или
-# слева направо главное помнить что он начнётся с элемента
-# start, по умолчанию шаг 1, если указать только шаг -1
-# то массив перевернётся
-
-example_str = "Курс по Python на Coursera"
-
-print(example_str[9:])
-print(example_str[9:15])
-print(example_str[-8:-2])
-print(example_str[::-1])
-
+# иттерация по строке
+for elem in num_string:
+    print(elem, end="")
 
