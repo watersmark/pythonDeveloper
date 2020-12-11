9ff880d4c3745b105f5a15262f5c0aee452b983c
watersmark <watersmark@mail.ru>
2020-12-11 17:20:32 +0300

    pr4

--- Coursera/Practise/work1/main.py
+++ Coursera/Practise/work1/main.py
@@ -1,13 +1,9 @@
-def is_isogram(string):
-    myMap = dict()
+def friend(x):
 
-    for elem in string:
-        if myMap.get(elem[0].upper()) == None:
-            myMap[elem.upper()] = 1
-        else:
-            return False
+    massFriends = []
+    for elem in x:
+        massFriends.append(elem) if (len(elem) == 4) else None
 
-    return True
+    return massFriends
 
-print(is_isogram("Dermatoglyphics"))
-print(is_isogram("moOse"))
+print(friend(["Ryan", "Kieran", "Mark"]))
