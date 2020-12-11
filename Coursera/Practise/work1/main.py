66e33ce25a63f2ba379c743f082708ac7f4bd937
watersmark <watersmark@mail.ru>
2020-12-11 16:58:31 +0300

    a

--- Coursera/Practise/work1/main.py
+++ Coursera/Practise/work1/main.py
@@ -1,4 +1,13 @@
-def solution(strArg):
-    return strArg[::-1]
+def to_jaden_case(string):
+    stringResult = ""
 
-print(solution("good"))
+    countMass = 1
+    countSplit = len(string.split(" "))
+
+    for elem in string.split(" "):
+        stringResult += elem[0].upper() + elem[1:]
+        stringResult += "" if (countMass == countSplit) else " "
+        countMass += 1
+    return stringResult
+
+print(to_jaden_case("How can mirrors be real if our eyes aren't real"))
