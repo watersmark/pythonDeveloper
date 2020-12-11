04f481c8d2c691ca311d2545d6eeda1225bc3a7f
watersmark <watersmark@mail.ru>
2020-12-11 17:14:07 +0300

    a

--- Coursera/Practise/work1/main.py
+++ Coursera/Practise/work1/main.py
@@ -1,13 +1,13 @@
-def to_jaden_case(string):
-    stringResult = ""
+def is_isogram(string):
+    myMap = dict()
 
-    countMass = 1
-    countSplit = len(string.split(" "))
+    for elem in string:
+        if myMap.get(elem[0].upper()) == None:
+            myMap[elem.upper()] = 1
+        else:
+            return False
 
-    for elem in string.split(" "):
-        stringResult += elem[0].upper() + elem[1:]
-        stringResult += "" if (countMass == countSplit) else " "
-        countMass += 1
-    return stringResult
+    return True
 
-print(to_jaden_case("How can mirrors be real if our eyes aren't real"))
+print(is_isogram("Dermatoglyphics"))
+print(is_isogram("moOse"))
