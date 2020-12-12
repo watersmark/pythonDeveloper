072ecdce0e9462e683ed8c3cbe053ef21f5c316d
watersmark <watersmark@mail.ru>
2020-12-12 14:27:40 +0300

    add codewars pr5:

--- Coursera/Practise/work1/main.py
+++ Coursera/Practise/work1/main.py
@@ -1,11 +1,5 @@
-def spin_words(sentence):
-    resString = ""
+def filter_list(l):
+    return [elem for elem in l if isinstance(elem, int)]
 
-    countElem = 1
-    for elem in sentence.split(" "):
-        resString += elem if len(elem) < 5 else elem[::-1]
-        resString += " " if countElem != len(sentence.split(" ")) else ""
-        countElem += 1
-    return resString
+print(filter_list([1,2,'a','b']))
 
-print(spin_words("Welcome"))
