5606cdb7c156f7b7256bc4360c1dcfd83f23606c
watersmark <watersmark@mail.ru>
2020-12-13 14:43:14 +0300

    add theory

--- Coursera/Practise/work1/main.py
+++ Coursera/Practise/work1/main.py
@@ -1,12 +1,35 @@
-def unique_in_order(iterable):
-    massSet = []
+# python коллекции
 
-    for index in range(len(iterable)):
-        if index == 0:
-            massSet.append(iterable[index])
-        else:
-            massSet.append(iterable[index]) if iterable[index - 1] != iterable[index] else None
+# рассмотрим список и его методы
+# данными способами можно инициализировать list
+empty_list = []
+empty_list = list()
+user_list = [1, ['name', True], 23.45]
 
-    return massSet
+# срез списка (последний элемент
+# не входит в список
+# при получени среза получается новый объект
+print(user_list[1:2])
 
-print(unique_in_order('AAAABBBCCDAABBB'))
+# добавление и расширение списков
+tempList = ["new", "list"]
+user_list.extend(tempList)  # расширение
+print(user_list)
+
+tempList.append(5)  # добавление
+print(tempList)
+
+tempList += 'a'  # ущё один способ добавления
+
+# метод для форматирования списков join
+print(", ".join([str(elem) for elem in tempList]))
+
+# сортировка в list
+# list.sort сортирует исходный список возвращвет None
+# sorted(list) возвращает отсортированную копию списка
+tempSort = [1, 3, 2, 0, 8, 6]
+
+print(sorted(tempSort, reverse=True))  # вернулась копия
+
+tempSort.sort(reverse=True)  # обратный порядок сортировки
+print(tempSort)
