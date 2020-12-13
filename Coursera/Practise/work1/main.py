81a009ef0f16bb7bef83cd6d4071fe1a547bb3a9
watersmark <watersmark@mail.ru>
2020-12-13 15:55:54 +0300

    add th

--- Coursera/Practise/work1/main.py
+++ Coursera/Practise/work1/main.py
@@ -1,29 +1,10 @@
-# словари dict()
+# чтобы словарь мог помнить порядок вхождения элементов
 
-# словари хэшируются получаем значения за константное время
-empty_dict = {}
-empty_dict = dict()
+from  collections import  OrderedDict
 
-# если обратиться к словарю по ключу
-# которого не существует, то будет ошибка
-# для этого используем метод get
-testMap = dict([[1, 2], ['a', 3], ['a', 4]])
-print(testMap.get(7, 'not found'))
+ordered = OrderedDict()
+for number in range(10):
+    ordered[number] = str(number)
 
-# в словарь можно добавлять и удалаять элементы
-testMap['test'] = 32 # удаление элемента
-print(testMap)
-
-del testMap['a']  # удаление элмента
-print(testMap)
-
-# удаляем элемент и возвращаем его значение
-print(testMap.pop(1))
-
-
-# для иттерации по словарю
-testMap = dict([['a', 11], ['b', 12], [32, 'ds']])
-
-for elem in testMap.items():
-    print(elem[1])
-
+for key in ordered.keys():
+    print(key)
