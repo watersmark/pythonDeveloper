fefda8261d6422423336eac5e47df6e6b4c62e91
watersmark <watersmark@mail.ru>
2020-12-14 13:05:57 +0300

    add 13

--- Coursera/Practise/work1/main.py
+++ Coursera/Practise/work1/main.py
@@ -1,60 +1,15 @@
-# функции
+# для чтения из файла, так же применяются
+# команды readline и readlines
+# readline считывает строку до символа ереноса строки
+# readlines считывает все строки в массив строк
 
-# аргументы по умолчанию
-def function_greet(name='unknown'):
-    print(name)
+file = open('test.txt', 'r')
+for elem in file.readlines():
+    print(elem)
 
+file.close()
 
-function_greet()
-
-# аргументы по умолчанию для каждой функции
-# интерпретатор записывает в особые переменные
-# поэтому изменяемы типы данных лучше не использовать, как значения по
-# умолчанию, так как они могут быть дозаписаны
-
-def append_one(arg=[]):
-    arg.append(1)
-    return arg
-
-print(append_one())
-print(append_one())
-
-# надо было сделать тип по умолчанию, как None
-def function(iterable=None):
-    iterable = iterable or []
-    return iterable
-
-print(function())
-
-
-# в python функции могут принимать неограниченное
-# кол-во аргументов
-# чтобы задать произвольнео число аргументов
-# используем звёздочку в функции
-# чтобы развернуть произвольное число аргументов
-# так же используем звёздочку
-# при разименовании словаря мы получаем озиционые аргументы
-# и можем передать их в *args
-def printer(*args):
-
-    print(args) # кортеж
-    print(*args) # отдельные элементы
-
-printer(1, 3, 4, 5)
-
-
-def print_arg(**args):
-    print(args) # словарь ключ-значение
-
-    for elem in args.items():   # получили отдельные значения
-        print("{key}, {value}".format(key=elem[0], value=elem[1]))
-
-print_arg(a=10, b=12)
-
-
-
-
-
-
-
-
+# для удобной работы с файлами используем
+with open('test.txt', 'r') as f:
+    for line in f:  # считываем файл построчно можно было указать f.readlines()
+        print(line)
