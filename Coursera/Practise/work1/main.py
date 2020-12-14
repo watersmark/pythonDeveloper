3ca9607651ee071083eb799bf7ed2ea9411309bd
watersmark <watersmark@mail.ru>
2020-12-14 17:24:50 +0300

    add all

--- Coursera/Practise/work1/main.py
+++ Coursera/Practise/work1/main.py
@@ -1,15 +1,11 @@
-# для чтения из файла, так же применяются
-# команды readline и readlines
-# readline считывает строку до символа ереноса строки
-# readlines считывает все строки в массив строк
+import json
+import functools
 
-file = open('test.txt', 'r')
-for elem in file.readlines():
-    print(elem)
 
-file.close()
+def to_json(func):
 
-# для удобной работы с файлами используем
-with open('test.txt', 'r') as f:
-    for line in f:  # считываем файл построчно можно было указать f.readlines()
-        print(line)
+    @functools.wraps(func)
+    def json_converter(*args, **kwargs):
+        return json.dumps(func(*args, **kwargs))
+
+    return json_converter
